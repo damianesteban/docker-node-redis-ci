3544ff85e8a881c9de7efde0b8c5350c392de60a
Damian Esteban <damian.esteban@gmail.com>
2017-02-13 22:52:56 -0500

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+const hapi = require('hapi');
+
+const server = new hapi.Server();
+server.connection({ port: 3000 });
+
+server.route([{
+  method: 'GET',
+  path: '/api/home',
+  handler: function (request, reply) {
+    reply({ message: 'Hello Balls' })
+  }
+}]);
+
+server.start(() => {
+  console.log(`Server is listening at: ${server.info.uri}`)
+})
